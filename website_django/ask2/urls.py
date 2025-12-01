a69dad51f7b104e0b899a1b043133466f735f405
joaotovolli <joaotovolli@hotmail.com>
2025-12-01 14:18:41 +0000

    Add Django Ask2 proxy on VM2 to call VM1 API

---
+++ website_django/ask2/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("ask2/", views.ask2_page, name="ask2_page"),
+    path("ask2/api/", views.ask2_api, name="ask2_api"),
+]
--- website_django/core/urls.py
+++ website_django/core/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("admin/", admin.site.urls),
-    path("ask2/", views.ask2_chat_page, name="ask2_chat_page"),
-    path("ask2/api/", views.ask2_chat_api, name="ask2_chat_api"),
+    path("", include("ask2.urls")),
 ]
